4705d04f78685b76bb6e27793bed7e53d1f479a1
Somē Cho <itssomicho@gmail.com>
2023-05-27 16:45:49 +0200

    fix tags

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -14,9 +14,9 @@ const Article = ({ markdown }) => {
 				children={markdown}
 				components={{
 					h1: ({ ...props }) => <h1 className="font-bold text-4xl my-8" {...props} />,
-					h2: ({ ...props }) => <h1 className="font-bold text-2xl mt-6" {...props} />,
+					h2: ({ ...props }) => <h2 className="font-bold text-2xl mt-6" {...props} />,
 					h3: ({ ...props }) => <h3 className="font-bold text-xl mt-6" {...props} />,
-					h4: ({ ...props }) => <h3 className="font-semibold text-md mt-3" {...props} />,
+					h4: ({ ...props }) => <h4 className="font-semibold text-md mt-3" {...props} />,
 					p: ({ ...props }) => <p className="my-4" {...props} />,
 					code: ({ ...props }) => <code className="bg-neutral-200 px-1 font-mono text-sm" {...props} />,
 					li: ({ ...props }) => <li className="list-disc list-inside" {...props} />,
