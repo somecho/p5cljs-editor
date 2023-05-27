c74a398be5adf0994de169dc98f74d73c66af173
Somē Cho <itssomicho@gmail.com>
2023-05-27 14:31:25 +0200

    use p5 module instead of cdn

--- src/App.integration.test.jsx
+++ src/App.integration.test.jsx
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { render, cleanup, screen } from '@testing-library/react'
+import { render, cleanup, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -13,6 +14,48 @@ global.fetch = jest.fn(() => Promise.resolve({
 	}
 }))
 
-it('renders the app', () => {
-	const wrapper = render(<App />, { wrapper: BrowserRouter })
+jest.mock('./lib/cljs', () => {
+	const originalModule = jest.requireActual('./lib/cljs')
+	return {
+		__esModule: true,
+		...originalModule,
+		compile: jest.fn((source) => `
+			cljs.user.setup = (function cljs$user$setup(){
+				return createCanvas((400),(400));
+			});
+			cljs.user.draw = (function cljs$user$draw(){
+			return background((220));
+			});`)
+	}
+})
+
+beforeEach(() => {
+	render(<App />, { wrapper: BrowserRouter })
+})
+
+describe('App integration test', () => {
+
+	it('checks the UI is complete', () => {
+		expect(screen.getByText('p5.cljs')).toBeInTheDocument()
+		expect(screen.getByText('run')).toBeInTheDocument()
+		expect(screen.getByText('stop')).toBeInTheDocument()
+		expect(screen.getByText('Editor')).toBeInTheDocument()
+		expect(screen.getByText('About')).toBeInTheDocument()
+		expect(screen.getByText('Tutorial')).toBeInTheDocument()
+	})
+
+	it('checks the holder divs are present', () => {
+		expect(document.getElementById('p5-script')).toBeTruthy()
+		expect(document.getElementById('user-script')).toBeTruthy()
+	})
+
+	it('starts the sketch', () => {
+		window.cljs = { ...window.cljs, user: {} }
+
+		screen.getByText('run').click()
+		console.log(document.getElementById('editor').innerHTML)
+		expect(document.getElementById('defaultCanvas0')).toBeTruthy()
+
+	})
+
 })
