8ca842900f7a3af87ff8b0861081d7bf1fe9edbb
Somē Cho <itssomicho@gmail.com>
2023-05-28 15:50:01 +0200

    setup cdn input

--- src/components/CDNModal.jsx
+++ src/components/CDNModal.jsx
@@ -5,25 +5,63 @@ import {
 	ModalHeader,
 	ModalFooter,
 	ModalBody,
-	ModalCloseButton,
+	Flex,
+	Button,
+	Input,
+	Text,
+	Spacer
 } from '@chakra-ui/react'
+import { useState } from 'react';
 
 const CDNModal = ({ isOpen, onClose }) => {
+	const [cdnLinks, setCdnLinks] = useState([])
+	const [userInput, setUserInput] = useState("")
+
+	function onAdd() {
+		setCdnLinks([...cdnLinks, userInput])
+		setUserInput("")
+	}
+
+	function onDelete(i) {
+		setCdnLinks(cdnLinks.filter((_, j) => i != j))
+	}
+
 	return (
-		<Modal isOpen={isOpen} onClose={onClose}>
+		<Modal isOpen={isOpen} onClose={onClose} size="2xl">
 			<ModalOverlay>
 				<ModalContent>
 					<ModalHeader>Add CDN</ModalHeader>
 					<ModalBody>
-						BODY
+						{cdnLinks.map((cdnLink, i) => (
+							<Flex key={cdnLink} align="center">
+								<Text>
+									{cdnLink}
+								</Text>
+								<Spacer />
+								<Button
+									variant='ghost'
+									onClick={() => { onDelete(i) }}
+								>×</Button>
+							</Flex>)
+						)}
+						<Input my="2" value={userInput} onChange={e => setUserInput(e.target.value)} />
 					</ModalBody>
 					<ModalFooter>
-						FOOTER
+						<Button onClick={() => { onClose() }}>
+							close
+						</Button>
+						<Button
+							backgroundColor="pink.500"
+							color="white"
+							ml="4"
+							onClick={() => { onAdd() }}
+						>
+							add
+						</Button>
 					</ModalFooter>
 				</ModalContent>
 			</ModalOverlay>
-
-		</Modal>
+		</Modal >
 	)
 
 }
